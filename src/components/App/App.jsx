71569dbf7e20241ae09ed53aab6f345670692cff
Dmytro <d.salyakin@gmail.com>
2025-02-22 14:42:28 +0200

    Add Profile's component markup

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,29 @@
+import Profile from '../Profile/Profile';
 import css from './App.module.css';
+
+const userData = {
+  username: "Jacques Gluke",
+  tag: "jgluke",
+  location: "Ocho Rios, Jamaica",
+  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308
+  }
+};
 
 export default function App() {
   return (
     <>
       <div className={css.container}>
-
+        <Profile
+          name={userData.username}
+          tag={userData.tag}
+          location={userData.location}
+          image={userData.avatar}
+          stats={userData.stats}
+        />
       </div>
     </>
   );
