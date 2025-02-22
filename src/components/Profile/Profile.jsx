da18092c46bb57a334b27c0e09521390bed2fabe
Dmytro <d.salyakin@gmail.com>
2025-02-22 19:32:33 +0200

    Complete Profile component styling

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,26 +1,28 @@
+import css from './Profile.module.css'
+
 export default function Profile({ name, tag, location, image, stats }) {
   return (
-    <div>
-      <div>
-        <img src={image} alt="User avatar" />
-        <p>{name}</p>
-        <p>@{tag}</p>
-        <p>{location}</p>
+    <div className={css.userCard}>
+      <div className={css.userInfo}>
+        <img className={css.userImg} src={image} alt="User avatar" />
+        <p className={css.userName}>{name}</p>
+        <p className={css.userOtherText}>@{tag}</p>
+        <p className={css.userOtherText}>{location}</p>
       </div>
-      <ul>
-        <li>
+      <ul className={css.statsList}>
+        <li className={css.statsListItem}>
           <span>Follovers</span>
           <span>{stats.followers}</span>
         </li>
-        <li>
+        <li className={css.statsListItem}>
           <span>Views</span>
           <span>{stats.views}</span>
         </li>
-        <li>
+        <li className={css.statsListItem}>
           <span>Likes</span>
           <span>{stats.likes}</span>
         </li>
       </ul>
-    </div>
+    </div >
   );
 }
