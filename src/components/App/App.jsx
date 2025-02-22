df02086d075481e69687654c31ee666865256ee3
Dmytro <d.salyakin@gmail.com>
2025-02-22 15:14:08 +0200

    Extract user data into userData.json

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,6 @@
+import userData from '../../userData.json';
 import Profile from '../Profile/Profile';
 import css from './App.module.css';
-
-const userData = {
-  username: "Jacques Gluke",
-  tag: "jgluke",
-  location: "Ocho Rios, Jamaica",
-  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-  stats: {
-    followers: 5603,
-    views: 4827,
-    likes: 1308
-  }
-};
 
 export default function App() {
   return (
